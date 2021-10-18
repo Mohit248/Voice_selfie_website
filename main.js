de10104c996c5d2a1f2d6c6745f610ba29e14730
Mohit248 <83233932+Mohit248@users.noreply.github.com>
2021-10-18 20:32:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,7 +24,10 @@ function speak(){
     var utter_This = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utter_This);
     Webcam.attach(camera);
-
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000)
 }
 
 camera = document.getElementById("camera");
@@ -34,3 +37,16 @@ Webcam.set({
     image_format:"png",
     png_quality:90
 });
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
